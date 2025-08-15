0709c91f4ba5b7d5134c0bdf1a3a70a50583a6db
karthikshetty <karthikashetty036@gmail.com>
2025-08-15 15:53:03 +0530

    Update mongoDB.js

--- backend/connection/mongoDB.js
+++ backend/connection/mongoDB.js
@@ -7,11 +7,12 @@ console.log("Trying to connect to MongoDB...");
 
 const connection = async () => {
     try {
-        await mongoose.connect("mongodb+srv://karthikashetty036:8546993820@cluster0.kvy4ulg.mongodb.net/");
+        await mongoose.connect(" add ur connection string here");
         console.log("Connected to MongoDB");
     } catch (err) {
         console.log("MongoDB not connected:", err);
     }
 };
+
 
 connection();
